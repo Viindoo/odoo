34f80dfe5ebb0ba51b4c4c2516576ec05f1e15bd
Roy Le <roy.le@viindoo.com>
2026-06-23 15:53:31 +0700

    [IMP] mail: improve the systray performance

--- addons/mail/models/res_users.py
+++ addons/mail/models/res_users.py
@@ -198,31 +198,56 @@ class Users(models.Model):
 
     @api.model
     def systray_get_activities(self):
-        activities = self.env["mail.activity"].search([("user_id", "=", self.env.uid)])
-        activities_by_record_by_model_name = defaultdict(lambda: defaultdict(lambda: self.env["mail.activity"]))
-        for activity in activities:
-            record = self.env[activity.res_model].browse(activity.res_id)
-            activities_by_record_by_model_name[activity.res_model][record] += activity
-        model_ids = list({self.env["ir.model"]._get(name).id for name in activities_by_record_by_model_name.keys()})
+        self.env["mail.activity"].flush_model(
+            ["res_model_id", "res_model", "res_id", "date_deadline", "user_id"])
+        today = fields.Date.context_today(self)
+        self.env.cr.execute("""
+            SELECT res_model_id, res_model
+              FROM mail_activity
+             WHERE user_id = %s AND res_model_id IS NOT NULL
+          GROUP BY res_model_id, res_model
+        """, [self.env.uid])
+        model_rows = self.env.cr.fetchall()
+        model_ids = [r[0] for r in model_rows]
+        IrModel = self.env["ir.model"].sudo()
         user_activities = {}
-        for model_name, activities_by_record in activities_by_record_by_model_name.items():
-            domain = [("id", "in", list({r.id for r in activities_by_record.keys()}))]
-            allowed_records = self.env[model_name].search(domain)
-            if not allowed_records:
+        for res_model_id, model_name in model_rows:
+            if not model_name or model_name not in self.env:
                 continue
-            module = self.env[model_name]._original_module
+            Model = self.env[model_name]
+            if not Model.check_access_rights("read", raise_exception=False):
+                continue
+            rule_query = Model._where_calc([])
+            Model._apply_ir_rules(rule_query, "read")
+            from_clause, where_clause, where_params = rule_query.get_sql()
+            allowed_sub = 'SELECT "%s".id FROM %s' % (Model._table, from_clause)
+            if where_clause:
+                allowed_sub += " WHERE " + where_clause
+            self.env.cr.execute("""
+                SELECT COUNT(*) FILTER (WHERE a.date_deadline < %%s),
+                       COUNT(*) FILTER (WHERE a.date_deadline = %%s),
+                       COUNT(*) FILTER (WHERE a.date_deadline > %%s)
+                  FROM mail_activity a
+                 WHERE a.user_id = %%s AND a.res_model = %%s
+                   AND a.res_id IN (%s)
+            """ % allowed_sub,
+                [today, today, today, self.env.uid, model_name] + list(where_params))
+            overdue_count, today_count, planned_count = self.env.cr.fetchone()
+            if not (overdue_count or today_count or planned_count):
+                continue
+            module = Model._original_module
             icon = module and modules.module.get_module_icon(module)
-            model = self.env["ir.model"]._get(model_name).with_prefetch(model_ids)
+            model = IrModel.browse(res_model_id).with_prefetch(model_ids)
             user_activities[model_name] = {
                 "id": model.id,
                 "name": model.name,
                 "model": model_name,
                 "type": "activity",
                 "icon": icon,
-                "total_count": 0,
-                "today_count": 0,
-                "overdue_count": 0,
-                "planned_count": 0,
+                "total_count": overdue_count + today_count,
+                "today_count": today_count,
+                "overdue_count": overdue_count,
+                "planned_count": planned_count,
                 "actions": [
                     {
                         "icon": "fa-clock-o",
@@ -230,11 +255,4 @@ class Users(models.Model):
                     }
                 ],
             }
-            for record, activities in activities_by_record.items():
-                if record not in allowed_records:
-                    continue
-                for activity in activities:
-                    user_activities[model_name]["%s_count" % activity.state] += 1
-                    if activity.state in ("today", "overdue"):
-                        user_activities[model_name]["total_count"] += 1
         return list(user_activities.values())
